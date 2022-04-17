77f8a46285d3d720669f3bcbf4246faef5164f9f
Sergei Kerencev <Kerentzev@bk.ru>
2022-04-17 10:37:37 +0300

    Добавил боковое меню и переделал toolbar по всем правилам

--- app/src/main/java/com/kerencev/notes/ui/MainActivity.java
+++ app/src/main/java/com/kerencev/notes/ui/MainActivity.java
@@ -1,16 +1,64 @@
 package com.kerencev.notes.ui;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
 import com.kerencev.notes.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ToolbarHolder {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigation);
+
+        setNavigationClicks();
+    }
+
+    @Override
+    public void setToolbar(Toolbar toolbar) {
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        actionBarDrawerToggle.syncState();
+
+    }
+
+    private void setNavigationClicks() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.notes:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_container, new NotesDescriptionFragment())
+                                .commit();
+                        drawerLayout.close();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/kerencev/notes/ui/ToolbarHolder.java
@@ -0,0 +1,8 @@
+package com.kerencev.notes.ui;
+
+import androidx.appcompat.widget.Toolbar;
+
+public interface ToolbarHolder {
+
+    void setToolbar(Toolbar toolbar);
+}
